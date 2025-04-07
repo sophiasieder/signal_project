8558f26a20f8fe110986e808408d41d520e8a887
sophiasieder <s.sieder@student.maastrichtuniversity.nl>
2025-04-07 17:27:32 +0200

    added Java Doc comments

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,15 @@
 package com.cardio_generator.outputs;
 
+/**
+ * interface that gives the structure for an output strategy, how and where the health care data will be outputcd
+ */
 public interface OutputStrategy {
+    /**
+     * method that needs to be overridden when interface is implemented
+     * @param patientId initializes the specific patient by the ID
+     * @param timestamp when the data was generated
+     * @param label describes the type of data
+     * @param data actual data
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
