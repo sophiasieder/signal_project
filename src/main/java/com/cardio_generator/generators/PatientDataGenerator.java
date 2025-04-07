8134d9acddb9cf883bbf03e7bdefb3129ee76c1d
sophiasieder <s.sieder@student.maastrichtuniversity.nl>
2025-04-07 17:21:04 +0200

    added Java Doc comments to health data simulator

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -2,6 +2,15 @@ package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+*interface to identify the structure for generating patient-specific health data
+ */
+
 public interface PatientDataGenerator {
+    /**
+     * generates and specifies health data for a specific patient
+     * @param patientId takes the ID of the patient that is being looked at
+     * @param outputStrategy how the data is being stored/where it goes (console, file etc.)
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
