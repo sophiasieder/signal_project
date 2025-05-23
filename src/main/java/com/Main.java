4a2fd5fcd6c480a7324749f3bd6df576952c6713
sophiasieder <s.sieder@student.maastrichtuniversity.nl>
2025-05-23 19:56:20 +0200

    added functionalities of section technical pointers

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+       /* if (args.length > 0 && args[0].equals("DataStorage")) {
+            DataStorage.main(new String[]{});
+        } else {
+            HealthDataSimulator.main(new String[]{});
+        }
+    }*/
+}
+
+
+
+}
